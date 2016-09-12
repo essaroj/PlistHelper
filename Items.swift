65f2c6811f36ab8d3c1250bfa87b5baded0f7f4d
Saroj Raut <sarojraut09@gmail.com>
2016-09-12 11:43:29 +0100

    Add files via upload

---
+++ Items.swift
@@ -0,0 +1,37 @@
+ 
+//  GoogleItems.swift
+//  GrabTaxi
+//
+//  Created by Bikram on 5/25/15.
+//  Copyright (c) 2015 Bikram. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class Items: NSObject{
+
+    var name:String?
+    var address:String?
+    var distance : String?
+    var Location: CLLocation?
+    
+    
+    override init(){}
+    
+    required init(coder aDecoder: NSCoder) {
+        self.name = aDecoder.decodeObjectForKey("name") as? String
+        self.address = aDecoder.decodeObjectForKey("address") as? String
+        self.distance = aDecoder.decodeObjectForKey("distance") as? String
+        self.Location = aDecoder.decodeObjectForKey("Location") as? CLLocation
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        if name != nil{  aCoder.encodeObject(name, forKey: "name")}
+        if address != nil{ aCoder.encodeObject(address, forKey: "address")}
+        if distance != nil { aCoder.encodeObject(distance, forKey: "distance")}
+        if Location != nil { aCoder.encodeObject(Location, forKey: "Location")}
+    }
+}
+
+
